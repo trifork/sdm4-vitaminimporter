8a44fc8914053224f6aab80fe218cc254bb6f003
Jakob Færch <jrf@trifork.com>
2012-09-05 13:15:39 +0200

    Spec for Grunddata inkl. test samt tilretninger af tabeller

--- src/main/java/dk/nsi/sdm4/vitamin/recordspecs/VitaminRecordSpecs.java
+++ src/main/java/dk/nsi/sdm4/vitamin/recordspecs/VitaminRecordSpecs.java
@@ -1,31 +1,34 @@
 package dk.nsi.sdm4.vitamin.recordspecs;
 
 import dk.nsi.sdm4.core.persistence.recordpersister.RecordSpecification;
-import static dk.nsi.sdm4.core.persistence.recordpersister.RecordSpecification.field;
+
+import static dk.nsi.sdm4.core.persistence.recordpersister.FieldSpecification.field;
 
 public class VitaminRecordSpecs {
-
     public static final RecordSpecification GRUNDDATA_RECORD_SPEC = RecordSpecification.createSpecification("VitaminGrunddata", "drugID", 
-            field("drugID", 0),
-            field("varetype", 0),
-            field("varedeltype", 0),
-            field("alfabetSekvensplads", 0),
-            field("specNummer", 0),
-            field("formKode", 0),
-            field("formTekst", 0),
-            field("kodeYderligereFormOplysninger", 0),
-            field("styrkeTekst", 0),
-            field("styrkeNumerisk", 0),
-            field("styrkeEnhed", 0),
-            field("mtIndehaverKode", 0),
-            field("repraesentantDistributoerKode", 0),
-            field("atcKode", 0),
-            field("administrationsvejKode", 0),
-            field("trafikadvarsel", 0),
-            field("substitution", 0),
-            field("substitutionsgruppe", 0),
-            field("dosisdispensering", 0),
-            field("darantaenedato", 0),
+            field("drugID", 11).numerical(),
+            field("varetype", 2),
+            field("varedeltype", 2),
+            field("alfabetSekvensplads", 9),
+            field("specNummer", 5),
+		    field("navn", 30),
+            field("formTekst", 20),
+		    field("formKode", 7),
+		    field("kodeYderligereFormOplysninger", 7),
+            field("styrkeTekst", 20),
+            field("styrkeNumerisk", 10).numerical(),
+            field("styrkeEnhed", 3),
+            field("mtIndehaverKode", 6).numerical(),
+            field("repraesentantDistributoerKode", 6).numerical(),
+            field("atcKode", 8),
+            field("administrationsvejKode", 8),
+            field("trafikadvarsel", 1),
+            field("substitution", 1),
+		    field("blank", 3).doNotPersist(),
+            field("substitutionsgruppe", 4),
+            field("dosisdispensering", 1),
+		    field("blank", 8).doNotPersist(),
+		    field("karantaeneDato", 8),
             field("sletningsstatus", 0));
     
     public static final RecordSpecification FIRMADATA_RECORD_SPEC = RecordSpecification.createSpecification("VitaminFirmadata", "firmaID", 
---
+++ src/test/java/dk/nsi/sdm4/vitamin/recordspecs/VitaminGrunddataRecordSpecsTest.java
@@ -0,0 +1,106 @@
+package dk.nsi.sdm4.vitamin.recordspecs;
+
+import dk.nsi.sdm4.core.parser.SingleLineRecordParser;
+import dk.nsi.sdm4.core.persistence.recordpersister.Record;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VitaminGrunddataRecordSpecsTest {
+	private SingleLineRecordParser grunddataParser;
+	private Record record;
+
+	@Before
+	public void makeParsers() {
+		grunddataParser = new SingleLineRecordParser(VitaminRecordSpecs.GRUNDDATA_RECORD_SPEC);
+		String line ="28116103706SPNA              EcovagFlora                   vaginalkapsler,hårdeVAGKAPH                           0000000000   011366011366 G01AX14VA                                ";
+		record = grunddataParser.parseLine(line);
+	}
+
+	@Test
+	public void parsesDrugID() {
+		assertEquals(28116103706L, record.get("drugID"));
+	}
+
+	@Test
+	public void parsesVaretype() {
+		assertEquals("SP", record.get("varetype"));
+	}
+
+	@Test
+	public void parsesVaredeltype() {
+		assertEquals("NA", record.get("varedeltype"));
+	}
+
+	@Test
+	public void parsesAlfabetSekvensplads() {
+		assertEquals("", record.get("alfabetSekvensplads"));
+	}
+
+	@Test
+	public void parsesSpecNummer() {
+		assertEquals("", record.get("specNummer"));
+	}
+
+	@Test
+	public void parsesNavn() {
+		assertEquals("EcovagFlora", record.get("navn"));
+	}
+
+	@Test
+	public void parsesFormTekst() {
+		assertEquals("vaginalkapsler,hårde", record.get("formTekst"));
+	}
+
+	@Test
+	public void parsesFormKode() {
+		assertEquals("VAGKAPH", record.get("formKode"));
+	}
+
+	@Test
+	public void parsesKodeYderligereFormOplysinger() {
+		assertEquals("", record.get("kodeYderligereFormOplysninger"));
+	}
+
+	@Test
+	public void parsesStyrkeTekst() {
+		assertEquals("", record.get("styrkeTekst"));
+	}
+
+	@Test
+	public void parsesStyrkeNumerisk() {
+		assertEquals(0L, record.get("styrkeNumerisk"));
+	}
+
+	@Test
+	public void parsesStyrkeEnhed() {
+		assertEquals("", record.get("styrkeEnhed"));
+	}
+
+	@Test
+	public void parsesMtIndehaverKode() {
+		assertEquals(11366L, record.get("mtIndehaverKode"));
+	}
+
+	@Test
+	public void parsesRepraesentantDistributoerKode() {
+		assertEquals(11366L, record.get("repraesentantDistributoerKode"));
+	}
+
+	@Test
+	public void parsesAtcKode() {
+		assertEquals("G01AX14", record.get("atcKode"));
+	}
+
+	@Test
+	public void parsesAdministrationsvejKode() {
+		assertEquals("VA", record.get("administrationsvejKode"));
+	}
+
+	@Test
+	public void parsesKarantaeneDato() {
+		assertEquals("", record.get("karantaeneDato"));
+	}
+
+}
