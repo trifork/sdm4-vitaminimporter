eb14656808b6f3c14f7ef5c63d2e663dea32d7a8
Jakob Færch <jrf@trifork.com>
2012-09-21 12:43:26 +0200

    Ret navn på test-metode, organize imports og fjern compiler-warning om potentielt null

--- src/test/java/dk/nsi/sdm4/vitamin/integrationtest/AllfilesAreImportedIT.java
+++ src/test/java/dk/nsi/sdm4/vitamin/integrationtest/AllfilesAreImportedIT.java
@@ -28,12 +28,14 @@ import com.mysql.jdbc.Driver;
 import dk.nsi.sdm4.testutils.StatuspageChecker;
 import dk.nsi.sdm4.testutils.TestDbConfiguration;
 import dk.nsi.sdm4.vitamin.config.VitaminimporterApplicationConfig;
-import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,7 +45,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.sql.DataSource;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -72,7 +73,7 @@ public class AllfilesAreImportedIT {
 	JdbcTemplate jdbcTemplate;
 
 	@Test
-	public void statusPageReturns200OK() throws Exception {
+	public void afterImportDatabaseContainsTheExpectedNumberOfEntities() throws Exception {
 		StatuspageChecker checker = new StatuspageChecker("vitaminimporter");
 
 		StatuspageChecker.StatuspageResult lastResult = null;
@@ -89,7 +90,7 @@ public class AllfilesAreImportedIT {
 			Thread.sleep(5*1000);
 		}
 
-		fail("Status page did not contain SUCCESS after " + MAX_RETRIES + ", text was " + lastResult.responseBody);
+		fail("Status page did not contain SUCCESS after " + MAX_RETRIES + (lastResult != null ? ", text was " + lastResult.responseBody : ""));
 	}
 
 	private void assertDbContainsExpectedRows() {
